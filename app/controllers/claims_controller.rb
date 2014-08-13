2e2c2e1c9e5577af2b675da7294d875ca0a07f41
msafiullah (Nitrous.IO) <msafiullah@gmail.com>
2014-08-13 15:46:10 +0000

    Beautified view claims

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -13,34 +13,7 @@ class ClaimsController < ApplicationController
   end
 
   def viewclaim
-    role = session[:role]
-    if role == 'normal' || role == 'smusasec' || role == 'cbdmc'
       @normalclaims = Claim.where(userid:session[:userid], clubid:session[:club])
-
-    elsif role == 'clubfinsec'
-      if params[:status].nil? || params[:status] == 'all'
-        @normalclaims = Claim.where(:clubid => session[:club], :status => 1..5)
-      elsif params[:status]=='unsubmitted'
-        @normalclaims = Claim.where(:clubid => session[:club], :status => 1, :remarks => nil)
-      elsif params[:status]=='submitted'
-        @normalclaims = Claim.where(:clubid => session[:club], :status => 2..5, :remarks => nil)
-      elsif params[:status]=='rejected'
-        @normalclaims = Claim.where(:clubid => session[:club], :status => 1).where.not(:remarks => nil)
-        @hideremarks = true
-      end
-
-    elsif role == 'cbdfinsec'
-      @normalclaims = Claim.where(['clubid in (select clubid from clubs where clubtype = ?) and status > 2 and status < 6', session[:club]])
-      @cbdmcclaims = Claim.where(clubid:session[:club], status: 7..11)
-    elsif role == 'president'
-      @normalclaims = Claim.where(clubid:session[:club], status: 2..5)
-      @cbdmcclaims = Claim.where(clubid:session[:club], status: 8..11)
-      @claims = @normalclaims + @cbdmcclaims
-    elsif role == 'smusafinsec'
-      @cbdmcclaims = Claim.where(status: 9..11)
-      @smusasecclaims = Claim.where(status: 13..15)
-    end
-
   end
 
   def add
